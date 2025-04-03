d72c6097c7d85f1ab5da8e552a08132f5cf80873
HoangHuy-23 <hh.glorious@gmail.com>
2025-04-03 11:58:31 +0700

    file systems

---
+++ src/main/java/com/hh23/car4u/Car4UApplication.java
@@ -0,0 +1,13 @@
+package com.hh23.car4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Car4UApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Car4UApplication.class, args);
+	}
+
+}
